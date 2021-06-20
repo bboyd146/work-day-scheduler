ece2a6ec40f70824101281e854909e4ba6de7af1
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-06-19 23:32:27 -0500

    adding script page with variables

---
+++ Assets/js/script.js
@@ -0,0 +1,13 @@
+// Time Blocks
+const nineA = $('#hr9');
+const tenA = $('#hr10');
+const elevA = $('#hr11');
+const noonP = $('#hr12');
+const oneP = $('#hr13');
+const twoP = $('#hr14');
+const threeP = $('#hr15');
+const fourP = $('#hr16');
+const fiveP = $('#hr17');
+// placeholder for current time
+const currentT = $('#currentDa');
+const saveBtn = $('button');
