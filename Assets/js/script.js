85bce6aa663943da9404b4ed17a61f73eec8c8a4
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-06-22 22:13:46 -0500

    adding script

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,11 +13,11 @@ var a = parseInt(nineA);
 var b = parseInt(tenA);
 var c = parseInt(elevA);
 var d = parseInt(noonP);
-var e = parseInt(oneP)+12;
-var f = parseInt(twoP)+12;
-var g = parseInt(threeP)+12;
-var h = parseInt(fourP)+12;
-var i = parseInt(fiveP)+12;
+var e = parseInt(oneP) + 12;
+var f = parseInt(twoP) + 12;
+var g = parseInt(threeP) + 12;
+var h = parseInt(fourP) + 12;
+var i = parseInt(fiveP) + 12;
 
 // grabbing rows to change text background.
 var firstT = document.getElementById('task');
@@ -32,7 +32,7 @@ var nineT = document.getElementById('task8');
 
 // placeholder for current time
 var currentT = document.getElementById('currentDay');
-var saveBtn = $('button');
+var saveBtn = $('.saveBtn');
 
 // current time
 var rightNow = moment();
@@ -47,72 +47,170 @@ console.log(h);
 // loops for color changing time blocks
 if (a === hours) {
     firstT.classList.add('present');
+    firstT.classList.remove('past');
+    firstT.classList.remove('future');
 } else if (a > hours) {
-    firstT.classList.add('future')
+    firstT.classList.add('future');
+    firstT.classList.remove('present');
+    firstT.classList.remove('past');
 } else {
-    firstT.classList.add('past')
+    firstT.classList.add('past');
+    firstT.classList.remove('present');
+    firstT.classList.remove('future');
 }
 
 if (b === hours) {
     secondT.classList.add('present');
+    secondT.classList.remove('past');
+    secondT.classList.remove('future');
 } else if (b > hours) {
-    secondT.classList.add('future')
+    secondT.classList.add('future');
+    secondT.classList.remove('present');
+    secondT.classList.remove('past');
+
 } else {
-    secondT.classList.add('past')
+    secondT.classList.add('past');
+    secondT.classList.remove('present');
+    secondT.classList.remove('future');
 }
 
 if (c === hours) {
     thirdT.classList.add('present');
+    thirdT.classList.remove('past');
+    thirdT.classList.remove('future');
 } else if (c > hours) {
-    thirdT.classList.add('future')
+    thirdT.classList.add('future');
+    thirdT.classList.remove('past');
+    thirdT.classList.remove('present');
 } else {
-    thirdT.classList.add('past')
+    thirdT.classList.add('past');
+    thirdT.classList.remove('present');
+    thirdT.classList.remove('future');
 }
 
 if (d === hours) {
     fourthT.classList.add('present');
+    fourthT.classList.remove('past');
+    fourthT.classList.remove('future');
 } else if (d > hours) {
-    fourthT.classList.add('future')
+    fourthT.classList.add('future');
+    fourthT.classList.remove('past');
+    fourthT.classList.remove('present');
 } else {
-    fourthT.classList.add('past')
+    fourthT.classList.add('past');
+    fourthT.classList.remove('present');
+    fourthT.classList.remove('future');
 }
 
 if (e === hours) {
     fifthT.classList.add('present');
+    fifthT.classList.remove('past');
+    fifthT.classList.remove('future');
 } else if (e > hours) {
-    fifthT.classList.add('future')
+    fifthT.classList.add('future');
+    fifthT.classList.remove('past');
+    fifthT.classList.remove('present');
 } else {
-    fifthT.classList.add('past')
+    fifthT.classList.add('past');
+    fifthT.classList.remove('present');
+    fifthT.classList.remove('future');
 }
 
 if (f === hours) {
     sixT.classList.add('present');
+    sixT.classList.remove('past');
+    sixT.classList.remove('future');
 } else if (f > hours) {
-    sixT.classList.add('future')
+    sixT.classList.add('future');
+    sixT.classList.remove('past');
+    sixT.classList.remove('present');
 } else {
-    sixT.classList.add('past')
+    sixT.classList.add('past');
+    sixT.classList.remove('present');
+    sixT.classList.remove('future');
 }
 
 if (g === hours) {
     sevenT.classList.add('present');
+    sevenT.classList.remove('past');
+    sevenT.classList.remove('future');
 } else if (g > hours) {
-    sevenT.classList.add('future')
+    sevenT.classList.add('future');
+    sevenT.classList.remove('past');
+    sevenT.classList.remove('present');
 } else {
-    sevenT.classList.add('past')
+    sevenT.classList.add('past');
+    sevenT.classList.remove('present');
+    sevenT.classList.remove('future');
 }
 
 if (h === hours) {
     eighthT.classList.add('present');
+    eighthT.classList.remove('past');
+    eighthT.classList.remove('future');
 } else if (h > hours) {
-    eighthT.classList.add('future')
+    eighthT.classList.add('future');
+    eighthT.classList.remove('past');
+    eighthT.classList.remove('present');
 } else {
-    eighthT.classList.add('past')
+    eighthT.classList.add('past');
+    eighthT.classList.remove('future');
+    eighthT.classList.remove('present');
 }
 
 if (i === hours) {
     nineT.classList.add('present');
+    nineT.classList.remove('past');
+    nineT.classList.remove('future');
 } else if (i > hours) {
-    nineT.classList.add('future')
+    nineT.classList.add('future');
+    nineT.classList.remove('past');
+    nineT.classList.remove('present');
+
 } else {
-    nineT.classList.add('past')
+    nineT.classList.add('past');
+    nineT.classList.remove('present');
+    nineT.classList.remove('future');
 }
+
+var tasks = [];
+
+$('.saveBtn').on('click', function (event) {
+    
+
+    
+    var thisss = $(this).parent('tr')
+    console.log(thisss);
+    // thisss.find('textarea').first().val()
+    
+    console.log('element clicked')
+    var things = thisss.find('textarea');
+    console.log(things);
+    // console.log('element clicked')
+    // var things = $("textarea").val();
+    console.log(things);
+    // // var storageTasks = JSON.stringify(savedTasks);
+    localStorage.setItem("savedTasks", JSON.stringify(things));
+
+
+    // var savedTasks = {
+    //     firstT: firstT.val(),
+    //     secondT: secondT.val(),
+    //     thirdT: $(this).next().val(),
+    //     fourthT: $(this).next().val(),
+    //     fifthT: $(this).next().val(),
+    //     sixT: $(this).next().val(),
+    //     sevenT: $(this).next().val(),
+    //     eighthT: $(this).next().val(),
+    //     nineT: $(this).next().val()
+    //   };
+    // var value = $(this).siblings(".description").val();
+    // // var time = $(this).parent().attr("scope");
+    // var dateAdded = moment().format("dddd, MMMM Do");
+
+    // tasks.push({description: value, time: time, date: dateAdded});
+
+    // // save the value in localStorage as time
+    // localStorage.setItem("events", JSON.stringify(tasks));
+    
+})
